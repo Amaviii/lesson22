b955741a8ee9a11af1ada023ccdb30c78ec4f851
Gregory <lubeevgs2000@gmail.com>
2025-09-27 17:44:50 +0300

    homeTask-2

--- app/src/main/java/com/example/lesson22/MainActivity.kt
+++ app/src/main/java/com/example/lesson22/MainActivity.kt
@@ -16,5 +16,29 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
+
+        //Реализация отступов согласно заданию
+        binding?.recyclerView?.addItemDecoration(SpaceItemDecoration(20))
+
+        val list = listOf(
+            PostsData.AuthorText("Уильям Шекспир", "Самый известный писатель на земле"),
+            PostsData.ImageText(R.drawable.ic_launcher_foreground, "Картинка"),
+            PostsData.TextButton("Какой-то текст"),
+        )
+
+        val newList = listOf(
+            PostsData.AuthorText("Лев Толстой", "Один из самых извсетных писателей на земле"),
+            PostsData.ImageText(R.drawable.ic_launcher_foreground, "Картинка"),
+            PostsData.TextButton("Текст обновился"),
+        )
+
+        val adapter = MyAdapter(list)
+
+        binding?.recyclerView?.layoutManager = LinearLayoutManager(this)
+        binding?.recyclerView?.adapter = adapter
+
+        binding?.refreshBtn?.setOnClickListener {
+            adapter.updateList(newList)
         }
+    }
 }
---
+++ app/src/main/java/com/example/lesson22/PostsData.kt
@@ -0,0 +1,22 @@
+package com.example.lesson22
+
+sealed class PostsData {
+
+    data class AuthorText(
+        val name: String,
+        val text: String,
+        val id: Int = 432423,
+    ) : PostsData()
+
+    data class ImageText(
+        val imageResId: Int,
+        val text: String,
+        val id: Int = 56546,
+    ) : PostsData()
+
+    data class TextButton(
+        val text: String,
+        val id: Int = 76752,
+    ) : PostsData()
+
+}
